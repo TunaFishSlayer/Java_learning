31d128e070b933f2516369207365233f92d182c8
TunaFishSlayer <buiminhtuan2711hp@gmail.com>
2024-07-20 19:14:31 +0700

    Just started

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+    public static void main(String[] args) {
+      int i = 5;
+      String name = "John";
+      System.out.println(i+5);
+      System.out.println("Hello " + name); 
+      System.out.println("Hello World");
+    }
+  }
+  
+  
---
+++ Simple_Classes.java
@@ -0,0 +1,12 @@
+public class Simple_Classes {
+	public static void main(String[] args) {   
+    String name = "TunaFishSlayer";
+// Uppercase is important
+    int age = 20;
+    char gender ='m';
+    double desiredSalary = 80000.45;
+    boolean lookingForJob = true;
+    System.out.println(name);
+    System.out.println(age);
+	}
+}
