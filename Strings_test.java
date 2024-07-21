68eb787e12424b7a0d67c28aff717518157588fd
TunaFishSlayer <buiminhtuan2711hp@gmail.com>
2024-07-21 23:53:40 +0700

    strings in java is cool

---
+++ Strings_test.java
@@ -0,0 +1,15 @@
+public class Strings_test {
+    public static void main(String[] args){
+    String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+    String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+    String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+    System.out.println(line1.equals(line2));
+    System.out.println(line3.equals(line2));
+    int animals = 12;
+    String species = "zebra";
+    String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+    System.out.println(zooDescription);
+  }       
+}
+       
+
