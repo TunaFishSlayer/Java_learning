52c9dc9d0be87b3111be6d7e4c6747aaa54dea55
TunaFishSlayer <buiminhtuan2711hp@gmail.com>
2024-08-05 23:30:17 +0700

    arrays

--- Arrays/Newsfeed.java
+++ Arrays/Newsfeed.java
@@ -1,16 +1,21 @@
 public class Newsfeed {
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
     public int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles;
     
     public Newsfeed(){
-  
+      favoriteArticles = new String[10];
     }
+    
+    public void setFavoriteArticle(int favoriteIndex, String newArticle){
+      favoriteArticles[favoriteIndex] = newArticle;
+    }
+  
       
     public String[] getTopics(){
       return topics;
     }
     
-    // Create getFirstTopic() below
     public String getFirstTopic(){
       return topics[0];
     }
